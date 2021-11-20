a3217867ea0899b5e3834090ba957fec2ca78060
John Cheng <cheng.john@pm.me>
2021-11-20 16:24:31 +0200

    Did something about shopping view

--- lib/shop.dart
+++ lib/shop.dart
@@ -5,9 +5,8 @@ class Shop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final children = <Widget>[];
-    for (var i = 0; i < 10; i++) {
-      children.add(Card(
+    final children = <Widget>[
+      Card(
           color: Colors.amber[600],
           child: InkWell(
             onTap: () {
@@ -19,12 +18,12 @@ class Shop extends StatelessWidget {
                   ),
                   body: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
+                    children: const <Widget>[
                       Hero(
-                        tag: 'hero-rectangle-$i',
-                        child: const Image(
-                          image:
-                              NetworkImage('https://picsum.photos/250?image=9'),
+                        tag: 'item-090400-059',
+                        child: Image(
+                          image: NetworkImage(
+                              'https://www.marimekko.com/media/catalog/product/0/9/090400-059_X191868_10_1631714482.jpg?width=750&height=938&canvas=750:938&quality=100&bg-color=255,255,255&fit=bounds'),
                           fit: BoxFit.cover,
                           width: double.infinity,
                         ),
@@ -36,11 +35,12 @@ class Shop extends StatelessWidget {
             },
             child: Column(
               children: [
-                Expanded(
+                const Expanded(
                   child: Hero(
-                    tag: 'hero-rectangle-$i',
-                    child: const Image(
-                      image: NetworkImage('https://picsum.photos/250?image=9'),
+                    tag: 'item-090400-059',
+                    child: Image(
+                      image: NetworkImage(
+                          'https://www.marimekko.com/media/catalog/product/0/9/090400-059_X191868_10_1631714482.jpg?width=750&height=938&canvas=750:938&quality=100&bg-color=255,255,255&fit=bounds'),
                       fit: BoxFit.cover,
                       width: double.infinity,
                     ),
@@ -60,16 +60,297 @@ class Shop extends StatelessWidget {
                 )
               ],
             ),
-          )));
-    }
+          )),
+      Card(
+          color: Colors.amber[600],
+          child: InkWell(
+            onTap: () {
+              // Navigate to card
+              Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext context) => Scaffold(
+                  appBar: AppBar(
+                    title: const Text('second Page'),
+                  ),
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const <Widget>[
+                      Hero(
+                        tag: 'item-090221-059',
+                        child: Image(
+                          image: NetworkImage(
+                              'https://www.marimekko.com/media/catalog/product/0/9/090221-059_X192944_20_1631716068.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+            },
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Hero(
+                    tag: 'item-090221-059',
+                    child: Image(
+                      image: NetworkImage(
+                          'https://www.marimekko.com/media/catalog/product/0/9/090221-059_X192944_20_1631716068.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text("Location:"),
+                        const SizedBox(height: 8),
+                        Text("Price: ")
+                      ]),
+                )
+              ],
+            ),
+          )),
+      Card(
+          color: Colors.amber[600],
+          child: InkWell(
+            onTap: () {
+              // Navigate to card
+              Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext context) => Scaffold(
+                  appBar: AppBar(
+                    title: const Text('second Page'),
+                  ),
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const <Widget>[
+                      Hero(
+                        tag: 'item-090401-059',
+                        child: Image(
+                          image: NetworkImage(
+                              'https://www.marimekko.com/media/catalog/product/0/9/090401-059_X191682_10_1631714257.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+            },
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Hero(
+                    tag: 'item-090401-059',
+                    child: Image(
+                      image: NetworkImage(
+                          'https://www.marimekko.com/media/catalog/product/0/9/090401-059_X191682_10_1631714257.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text("Location:"),
+                        const SizedBox(height: 8),
+                        Text("Price: ")
+                      ]),
+                )
+              ],
+            ),
+          )),
+      Card(
+          color: Colors.amber[600],
+          child: InkWell(
+            onTap: () {
+              // Navigate to card
+              Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext context) => Scaffold(
+                  appBar: AppBar(
+                    title: const Text('second Page'),
+                  ),
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const <Widget>[
+                      Hero(
+                        tag: 'item-090243-559',
+                        child: Image(
+                          image: NetworkImage(
+                              'https://www.marimekko.com/media/catalog/product/0/9/090243-559_X192616_20_1632900311.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+            },
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Hero(
+                    tag: 'item-090243-559',
+                    child: Image(
+                      image: NetworkImage(
+                          'https://www.marimekko.com/media/catalog/product/0/9/090243-559_X192616_20_1632900311.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text("Location:"),
+                        const SizedBox(height: 8),
+                        Text("Price: ")
+                      ]),
+                )
+              ],
+            ),
+          )),
+      Card(
+          color: Colors.amber[600],
+          child: InkWell(
+            onTap: () {
+              // Navigate to card
+              Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext context) => Scaffold(
+                  appBar: AppBar(
+                    title: const Text('second Page'),
+                  ),
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const <Widget>[
+                      Hero(
+                        tag: 'item-090361-099',
+                        child: Image(
+                          image: NetworkImage(
+                              'https://www.marimekko.com/media/catalog/product/0/9/090361-099_X191674_10_1629517569.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+            },
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Hero(
+                    tag: 'item-090361-099',
+                    child: Image(
+                      image: NetworkImage(
+                          'https://www.marimekko.com/media/catalog/product/0/9/090361-099_X191674_10_1629517569.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text("Location:"),
+                        const SizedBox(height: 8),
+                        Text("Price: ")
+                      ]),
+                )
+              ],
+            ),
+          )),
+      Card(
+          color: Colors.amber[600],
+          child: InkWell(
+            onTap: () {
+              // Navigate to card
+              Navigator.of(context).push(MaterialPageRoute<void>(
+                builder: (BuildContext context) => Scaffold(
+                  appBar: AppBar(
+                    title: const Text('second Page'),
+                  ),
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const <Widget>[
+                      Hero(
+                        tag: 'item-090276-003',
+                        child: Image(
+                          image: NetworkImage(
+                              'https://www.marimekko.com/media/catalog/product/0/9/090276-003_X191680_10_1634734283.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+            },
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Hero(
+                    tag: 'item-090276-003',
+                    child: Image(
+                      image: NetworkImage(
+                          'https://www.marimekko.com/media/catalog/product/0/9/090276-003_X191680_10_1634734283.jpg?quality=100&bg-color=255,255,255&fit=bounds&height=469&width=375&canvas=375:469'),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Text("Location:"),
+                        const SizedBox(height: 8),
+                        Text("Price: ")
+                      ]),
+                )
+              ],
+            ),
+          )),
+    ];
 
     return Scaffold(
         appBar: AppBar(
-          title: Text("Mari n Go"),
+          centerTitle: true,
+          titleTextStyle: const TextStyle(
+              color: Color(0xFF4960a3),
+              fontSize: 25,
+              fontWeight: FontWeight.bold),
+          title: const Text("Mari n Go"),
+          backgroundColor: Colors.white,
         ),
         body: GridView.count(
           crossAxisCount: 2,
-          childAspectRatio: 0.8,
+          childAspectRatio: 0.6,
           padding: const EdgeInsets.all(10.0),
           crossAxisSpacing: 5.0,
           mainAxisSpacing: 5.0,
